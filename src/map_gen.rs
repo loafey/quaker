18137148745e94a4c3e4d99c9558fd13ee4f42c8
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-02-26 23:47:24 +0100

    Try 500

--- src/map_gen.rs
+++ src/map_gen.rs
@@ -2,9 +2,9 @@ use bevy::{
     prelude::*,
     render::{render_asset::RenderAssetUsages, render_resource::PrimitiveTopology},
 };
-use macros::error_return;
+use macros::{error_return, npdbg};
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 struct Plane {
     n: Vec3,
     d: f32,
@@ -46,84 +46,18 @@ impl Plane {
             InPlane::In
         }
     }
-}
 
-fn get_intersection(p1: Plane, p2: Plane, p3: Plane) -> Option<Vec3> {
-    let denom = p1.n.dot(p2.n.cross(p3.n));
-    if denom.abs() < f32::EPSILON {
-        return None;
-    }
-    let p =
-        -p1.d * (p2.n.cross(p3.n)) - p2.d * (p3.n.cross(p1.n)) - p3.d * (p1.n.cross(p2.n)) / denom;
-    Some(p)
-}
-
-fn get_vertices(brush: &[Plane]) -> Vec<Vec3> {
-    let mut vertices = Vec::new();
-    for fi in brush {
-        for fj in brush {
-            if fi == fj {
-                continue;
-            }
-            for fk in brush {
-                if fk == fi || fk == fj {
-                    continue;
-                }
-
-                if let Some(val) = get_intersection(*fi, *fj, *fk) {
-                    let mut legal = true;
-                    for f in brush {
-                        if f.n.dot(val) + f.d > 0.0 {
-                            legal = false;
-                            break;
-                        }
-                    }
-
-                    if legal {
-                        vertices.push(val);
-                    }
-                }
-            }
+    pub fn get_intersection(&self, a: &Plane, b: &Plane) -> Option<Vec3> {
+        let denom = self.n.dot(a.n.cross(b.n));
+        match dbg!(denom.abs()) < f32::EPSILON {
+            true => None,
+            false => Some(
+                ((a.n.cross(b.n)) * -self.d
+                    - (b.n.cross(self.n)) * a.d
+                    - (self.n.cross(a.n)) * b.d)
+                    / denom,
+            ),
         }
-    }
-    vertices
-}
-
-fn sort_verticies_cw(vertices: &mut [Vec3], normal: Vec3) {
-    let mut center = Vec3::default();
-    for vec in vertices.iter() {
-        center += *vec;
-    }
-    center /= vertices.len() as f32;
-
-    for i in 0..vertices.len() - 2 {
-        let a = (vertices[i] - center).normalize();
-        let p = Plane::from_points(vertices[i], center, center + normal);
-
-        let mut smallest_angle = -1.0;
-        // assuming a mesh does not consist of 18446744073709551615
-        // or 4294967295 (for the 32bit scrubs) planes
-        let mut smallest = usize::MAX;
-        #[allow(clippy::needless_range_loop)]
-        for j in i + 1..vertices.len() - 1 {
-            if !matches!(p.classify_point(vertices[j]), InPlane::Back) {
-                let b = (vertices[j] - center).normalize();
-
-                let angle = a.dot(b);
-
-                if angle > smallest_angle {
-                    smallest_angle = angle;
-                    smallest = j;
-                }
-            }
-        }
-
-        if smallest == usize::MAX {
-            error!("degenerate polygon");
-            continue;
-        }
-
-        vertices.swap(smallest, i + 1);
     }
 }
 
@@ -138,49 +72,74 @@ pub fn test_map(
     for entity in map {
         for brush in entity.brushes {
             // Calculate the verticies for the mesh
-            let brush = brush.into_iter().map(Plane::from_data).collect::<Vec<_>>();
-            // wha??
-            let mut vertices = get_vertices(&brush);
-            // Calculate texture coordinates
-            warn!("Texture coordinates not implemented yet...");
+            let faces = brush.into_iter().map(Plane::from_data).collect::<Vec<_>>();
+            let polys = get_polys(faces);
+            println!("{polys:?}");
 
-            println!("{vertices:?}");
-
-            // Sort the vectors
-            for brush in &brush {
-                let normal = brush.n;
-                //sort_verticies_cw(&mut vertices, normal);
-            }
-
-            let mut verts = Vec::new();
-            for vertex in vertices {
-                let vertex = vertex / 80.0;
-
-                if cfg!(debug_assertions) {
-                    let cube = Cuboid::new(0.1, 0.1, 0.1);
-                    commands.spawn(PbrBundle {
-                        mesh: meshes.add(cube),
-                        material: materials.add(Color::rgb_u8(0, 255, 0)),
-                        transform: Transform::from_xyz(vertex.x, vertex.y, vertex.z),
-                        ..default()
-                    });
-                }
-                verts.push(vertex);
-            }
-
-            let mut new_mesh = Mesh::new(
-                PrimitiveTopology::TriangleList,
-                RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-            )
-            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, verts);
-            new_mesh.compute_flat_normals();
-
-            commands.spawn(PbrBundle {
-                mesh: meshes.add(new_mesh),
-                material: materials.add(Color::rgb_u8(255, 0, 0)),
-                transform: Transform::default(),
-                ..default()
-            });
+            //
+            // let mut new_mesh = Mesh::new(
+            //     PrimitiveTopology::TriangleList,
+            //     RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+            // )
+            // .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, verts);
+            // new_mesh.compute_flat_normals();
+            //
+            // commands.spawn(PbrBundle {
+            //     mesh: meshes.add(new_mesh),
+            //     material: materials.add(Color::rgb_u8(255, 0, 0)),
+            //     transform: Transform::default(),
+            //     ..default()
+            // });
         }
+    }
+}
+
+fn get_polys(faces: Vec<Plane>) -> Vec<Poly> {
+    let ui_faces = faces.len();
+    let mut polys = Vec::new();
+    for _ in 0..ui_faces {
+        polys.push(Poly::default());
+    }
+
+    for i in 0..faces.len() - 2 {
+        let fi = faces[i];
+        for j in (i + 1)..faces.len() - 1 {
+            let fj = faces[j];
+            #[allow(clippy::needless_range_loop)]
+            'k: for k in (j + 1)..faces.len() - 1 {
+                let fk = faces[k];
+
+                warn!("Should be 4 more here...");
+                if let Some(p) = npdbg!(fi.get_intersection(&fj, &fk)) {
+                    for f in &faces {
+                        if matches!(f.classify_point(p), InPlane::Front) {
+                            continue 'k;
+                        }
+                    }
+
+                    let v = Vertex::from_p(p);
+                    polys[i].verts.push(v);
+                    polys[j].verts.push(v);
+                    polys[k].verts.push(v);
+                }
+            }
+        }
+    }
+    polys
+}
+
+#[derive(Debug, Default)]
+struct Poly {
+    verts: Vec<Vertex>,
+    plane: Plane,
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+struct Vertex {
+    p: Vec3,
+}
+impl Vertex {
+    pub fn from_p(p: Vec3) -> Self {
+        Self { p }
     }
 }
