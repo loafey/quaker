4503f3b9f21974da93d770dcf974cd40087bd655
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-03-09 14:00:34 +0100

    Get player data on collision with player.

--- src/entities/pickup.rs
+++ src/entities/pickup.rs
@@ -1,7 +1,8 @@
-use crate::map_gen::entities::data::PickupData;
+use crate::{map_gen::entities::data::PickupData, player::Player};
 use bevy::{
     ecs::{
         component::Component,
+        entity::Entity,
         event::EventReader,
         system::{Query, Res},
     },
@@ -24,14 +25,18 @@ impl PickupEntity {
     }
     pub fn update(
         mut query: Query<(&mut PickupEntity, &mut Transform)>,
+        mut entities: Query<&mut Player>,
         mut reader: EventReader<CollisionEvent>,
         time: Res<Time>,
     ) {
         for event in reader.read() {
-            if let CollisionEvent::Started(e1, e2, CollisionEventFlags::SENSOR) = event {
-                println!("{e1:?} {e2:?}");
+            if let CollisionEvent::Started(_, e2, CollisionEventFlags::SENSOR) = event {
+                if let Ok(player) = entities.get_mut(*e2) {
+                    println!("{player:?}");
+                }
             }
         }
+
         for (_pe, mut trans) in query.iter_mut() {
             trans.rotate_y(time.delta_seconds());
         }
