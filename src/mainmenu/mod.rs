21a21d97fc4e263f9eb5d27260773baaa10ab13f
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-03-18 22:53:40 +0100

    Start on main menu.

--- src/mainmenu/mod.rs
+++ src/mainmenu/mod.rs
@@ -4,7 +4,6 @@ use bevy::prelude::*;
 pub struct MainMenuEnt;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Camera
     commands
         .spawn(Camera2dBundle::default())
         .insert(MainMenuEnt);
@@ -18,6 +17,38 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ..default()
             },
             ..default()
+        })
+        .with_children(|c| {
+            c.spawn((
+                TextBundle::from_section(
+                    "Quaker!",
+                    TextStyle {
+                        font_size: 100.0,
+                        ..default()
+                    },
+                )
+                .with_text_justify(JustifyText::Center)
+                .with_style(Style {
+                    position_type: PositionType::Absolute,
+                    left: Val::Px(5.0),
+                    top: Val::Px(5.0),
+                    ..default()
+                }),
+                Label,
+            ));
+
+            c.spawn(NodeBundle {
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    width: Val::Px(400.0),
+                    border: UiRect::all(Val::Px(2.0)),
+                    height: Val::Vh(100.0),
+                    right: Val::Px(0.0),
+                    ..default()
+                },
+                background_color: Color::rgb(0.65, 0.65, 0.65).into(),
+                ..default()
+            });
         })
         .insert(MainMenuEnt);
 }
