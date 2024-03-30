d089dea099fc0070a2eebffd1fefa0f14457c94d
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-03-30 01:14:43 +0100

    Remove comment and warning.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -63,7 +63,7 @@ pub fn update_world(client_id: u64, message: &ClientMessage, nw: &mut NetWorld) 
             }
         }
         ClientMessage::PickupWeapon { weapon } => {
-            for (player_ent, mut player, t) in nw.players.iter_mut() {
+            for (player_ent, mut player, _) in nw.players.iter_mut() {
                 if player.id == client_id {
                     if let Some(weapon_data) = nw.weapon_map.0.get(weapon) {
                         let slot = weapon_data.slot;
@@ -82,11 +82,6 @@ pub fn update_world(client_id: u64, message: &ClientMessage, nw: &mut NetWorld) 
                                     settings: PlaybackSettings::DESPAWN.with_spatial(true),
                                 });
                             });
-                            // nw.audio.play(nw.asset_server.load(
-                            //     weapon_data.pickup_sound.clone().unwrap_or(
-                            //         "sounds/Player/Guns/SuperShotgun/shotgunCock.ogg".to_string(),
-                            //     ),
-                            // ));
 
                             if player.id == nw.current_id.0 {
                                 player.display_message(
