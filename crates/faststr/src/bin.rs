8fda915cdfca206b7e9682cebff6b83a658c40bd
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-01 19:36:51 +0200

    Improve the FastStr test

--- crates/faststr/src/bin.rs
+++ crates/faststr/src/bin.rs
@@ -2,34 +2,38 @@ use std::time::Instant;
 
 use faststr::FastStr;
 
-const TEST_AMOUNT: usize = 10000;
-
 fn main() {
-    for c in (0..255u8).map(|c| c as char) {
-        let og_string = [c; TEST_AMOUNT].into_iter().collect::<String>();
-        println!("Testing {c:?}");
+    let max = 10000;
+    let chars = 255u8;
+    for test_amount in 0..max {
+        println!("Testing {test_amount}/{max}");
+        let mut string_total_time = 0.0;
+        let mut fstring_total_time = 0.0;
 
-        let mut veccy = Vec::new();
-        let time = Instant::now();
-        for _ in 0..TEST_AMOUNT {
-            veccy.push(og_string.clone());
+        for c in (0..chars).map(|c| c as char) {
+            let og_string = vec![c; test_amount].into_iter().collect::<String>();
+
+            let mut veccy = Vec::new();
+            let time = Instant::now();
+            for _ in 0..test_amount {
+                veccy.push(og_string.clone());
+            }
+            string_total_time += time.elapsed().as_secs_f32();
+
+            let mut veccy = Vec::new();
+            let fstring = FastStr::from(og_string);
+            let time = Instant::now();
+            for _ in 0..test_amount {
+                veccy.push(fstring.clone());
+            }
+            fstring_total_time += time.elapsed().as_secs_f32();
         }
-        println!(
-            "Cloning String of {TEST_AMOUNT} {c:?} took {}s",
-            time.elapsed().as_secs_f32()
-        );
 
-        let mut veccy = Vec::new();
-        let fstring = FastStr::from(og_string);
-        let time = Instant::now();
-        for _ in 0..TEST_AMOUNT {
-            veccy.push(fstring.clone());
-        }
-        println!(
-            "Cloning FastStr of {TEST_AMOUNT} {c:?} took {}s",
-            time.elapsed().as_secs_f32()
-        );
-
+        let avg_string = string_total_time / chars as f32;
+        let avg_fstring = fstring_total_time / chars as f32;
+        println!("Copying String took {avg_string:.10}s on average",);
+        println!("Copying FastStr took {avg_fstring:.10}s on average",);
+        println!("A {:.2}% speedup", (avg_string / avg_fstring) * 100.0);
         println!()
     }
 }
