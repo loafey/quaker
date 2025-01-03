0275738764c5655ff390cd5ca5b6a9b646f3ec27
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-03 15:53:55 +0100

    Add map_interact function to plugins

--- crates/qwak_shared/lib.rs
+++ crates/qwak_shared/lib.rs
@@ -1,5 +1,7 @@
+#![allow(clippy::unused_unit)]
 #[qwak_macro::plugin]
 pub trait QwakPlugin {
     fn plugin_name() -> String;
     fn plugin_version() -> [i32; 3];
+    fn map_interact(arg: String) -> ();
 }
--- qwaks/default/src/lib.rs
+++ qwaks/default/src/lib.rs
@@ -3,10 +3,19 @@ qwak_shared::plugin_gen!(Plugin);
 struct Plugin;
 impl QwakPlugin for Plugin {
     fn plugin_name() -> String {
-        "Ondth default qwak".to_string()
+        "Ondth".to_string()
     }
 
     fn plugin_version() -> [i32; 3] {
         [0, 0, 1]
+    }
+
+    fn map_interact(arg: String) {
+        match &*arg {
+            "debug_log" => {
+                println!("yo")
+            }
+            _ => panic!("unknown interaction: {arg}"),
+        }
     }
 }
