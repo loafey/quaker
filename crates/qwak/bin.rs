144973f85fcf68b61634311cf8a57bb79bbda83b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-03 15:48:53 +0100

    Mutabilty fix

--- crates/qwak/bin.rs
+++ crates/qwak/bin.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let mut plugin =
-        qwak::QwakPlugin::new("target/wasm32-unknown-unknown/debug/default.wasm").unwrap();
-
+    let plugin = qwak::QwakPlugin::new("target/wasm32-unknown-unknown/debug/default.wasm").unwrap();
     println!("{:?}", plugin.plugin_name());
     println!("Running version: {:?}", plugin.plugin_version());
 }
