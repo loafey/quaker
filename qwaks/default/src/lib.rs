efa02389f33e0da1f84d2f2446bb202ef56837f1
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-04 00:54:30 +0100

    Add important detail

--- qwaks/default/src/lib.rs
+++ qwaks/default/src/lib.rs
@@ -9,6 +9,8 @@ unsafe extern "ExtismHost" {
     unsafe fn get_player_name(id: u64) -> String;
 }
 
+// Simple QWAK plugin that contains the required functions.
+// This is compiled to WASM.
 struct Plugin;
 impl QwakPlugin for Plugin {
     fn plugin_init() {}
@@ -22,12 +24,18 @@ impl QwakPlugin for Plugin {
     }
 
     fn map_interact(MapInteraction(arg, id): MapInteraction) {
-        match &*arg {
-            "debug_log" => unsafe {
-                let name = get_player_name(id).unwrap();
-                broadcast_message(format!("{name} is a crazy duck!")).unwrap()
-            },
-            _ => panic!("unknown interaction: {arg}"),
+        unsafe {
+            match &*arg {
+                "debug_log" => {
+                    let name = get_player_name(id).unwrap();
+                    let prefix = match &*name {
+                        "Felony" => "cooler",
+                        _ => "cool",
+                    };
+                    broadcast_message(format!("{name} is a {prefix} duck!")).unwrap()
+                }
+                _ => panic!("unknown interaction: {arg}"),
+            }
         }
     }
 }
