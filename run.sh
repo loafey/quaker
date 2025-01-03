032912fac941a29d4906dd2ced375a707cdf98d4
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-03 13:45:41 +0100

    Improve qwak build script

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
-cargo build -p default --target wasm32-unknown-unknown
-echo "    Compiled default QWAK file to: \"target/wasm32-unknown-unknown/debug/default.wasm\"" 
-cargo run -p qwak
+bold=$(tput bold)
+normal=$(tput sgr0)
+orange="\e[94m"
+
+cd qwaks
+folders=$(ls)
+cd ..
+mkdir -p assets/qwaks
+for proj in ${folders}; do
+    cargo build -p $proj --target wasm32-unknown-unknown --release
+    echo -e "    ${bold}${orange}Compiled${normal} \"$proj\" QWAK file to: \"target/wasm32-unknown-unknown/release/$proj.wasm\"" 
+    cp "target/wasm32-unknown-unknown/release/$proj.wasm" "assets/qwaks/$proj.wasm"
+    echo -e "      ${bold}${orange}Copied${normal} \"$proj\" QWAK file to asset directory" 
+done
+
+cargo run --release
